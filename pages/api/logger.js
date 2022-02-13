f5bea78406b2a06636887384774bd170f1065d77
Raul Oliva <oliva.raul12@gmail.com>
2022-02-12 20:18:39 -0600

    Creating the ConsoleLogger class for production env only

--- pages/api/logger.js
+++ pages/api/logger.js
@@ -1,11 +1,22 @@
-const winston = require("winston");
-const fs = require('fs') 
+const winston = require('winston');
+const fs = require('fs');
 const { createLogger, format } = winston;
 const { combine, timestamp, label, printf } = format;
 
-export const loggerConstructor = (labelText) => {
-  if(process.env.NODE_ENV == "production") {
-    return
+class ConsoleLogger {
+  info(msg) {
+    console.log('Info: ', msg);
+  }
+
+  error(msg) {
+    console.error('Error: ', msg);
+  }
+}
+
+export const loggerConstructor = labelText => {
+  if (process.env.NODE_ENV1 == 'production') {
+    const consoleLogger = new ConsoleLogger();
+    return consoleLogger;
   }
 
   fs.existsSync('logs') || fs.mkdirSync('logs');
@@ -16,15 +27,15 @@ export const loggerConstructor = (labelText) => {
   });
 
   const logger = createLogger({
-    level: "info",
+    level: 'info',
     format: format.json(),
-    format: combine(label({ label: "API" }), timestamp(), myFormat),
+    format: combine(label({ label: 'API' }), timestamp(), myFormat),
     transports: [
       new winston.transports.File({
-        filename: "logs/errors.log",
-        level: "error",
+        filename: 'logs/errors.log',
+        level: 'error',
       }),
-      new winston.transports.File({ filename: "logs/logs.log" }),
+      new winston.transports.File({ filename: 'logs/logs.log' }),
       new winston.transports.Console({ format: winston.format.simple() }),
     ],
   });
